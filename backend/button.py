aca5630baab9fa24a7d225d64965e96dde025500
MuzickMan80 <dale@megndale.com>
2021-12-12 13:37:02 -0600

    Enable pullup on reset button

--- backend/button.py
+++ backend/button.py
@@ -3,7 +3,7 @@ import inspect
 class Button:
     def __init__(self, io_manager, pin, handler):
         self.handler = handler
-        io_manager.monitor_pin(pin, self.on_pressed, True)
+        io_manager.monitor_pin(pin, self.on_pressed, falling=True, pullUp=True)
 
     async def on_pressed(self, edge, tick):
         result = self.handler()
