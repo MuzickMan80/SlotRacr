7cf37a566186966c22121c8953c7444b25f96fdb
MuzickMan80 <dale@megndale.com>
2022-01-08 22:40:44 -0600

    Add lane name settings

--- backend/racr/settings/race_settings.py
+++ backend/racr/settings/race_settings.py
@@ -1,8 +1,21 @@
 
+from racr.settings.setting import StringSetting
 from racr.settings.setting import BoolSetting
+
+enable_pitting=BoolSetting("Enable Pitting",True,"Require drivers to perform Pit Stops during the race")
+
+class LaneNameSetting(StringSetting):
+    def __init__(self, lane):
+        super().__init__(f"Lane {lane} Name")
 
 enable_pitting=BoolSetting("Enable Pitting",True,"Require drivers to perform Pit Stops during the race")
 
 race_settings={
     'enable_pitting': enable_pitting
 }
+
+lane_name=[]
+
+for i in range(8):
+    lane_name.append(LaneNameSetting(i+1))
+    race_settings[f'lane{i+1}_name'] = lane_name[i] 
