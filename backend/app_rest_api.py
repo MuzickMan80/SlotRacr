4358bc1c8d6b54a1d693a9aad5645675d42072eb
MuzickMan80 <dale@megndale.com>
2022-02-12 16:08:02 -0600

    Fix web frontend after moving working directory

--- backend/app_rest_api.py
+++ backend/app_rest_api.py
@@ -46,7 +46,7 @@ async def put_setting_property(request:Request):
 
 @routes.route('*', '/{tail:.*}')
 async def root_handler(request:Request):
-    root = '../web_frontend/dist/FrontEnd'
+    root = 'SlotRacr/web_frontend/dist/FrontEnd'
     path = f'{root}/{request.path}'
     if (os.path.isfile(path)):
         return web.FileResponse(path)
