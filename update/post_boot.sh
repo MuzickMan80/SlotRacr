45089912caab077eb3d8a3ccf2e6c93f244ad579
MuzickMan80 <dale@megndale.com>
2023-10-21 18:49:14 -0500

    Add dhcp server to eth0

--- update/post_boot.sh
+++ update/post_boot.sh
@@ -31,6 +31,23 @@ check_update_service backend slot_timer.service
 check_update_service update timer_update.service
 sudo systemctl enable pigpiod
 
+if ! grep -xF 'interface eth0' /etc/dhcpcd.conf
+then
+	echo 'interface eth0' >> /etc/dhcpcd.conf
+	echo 'static ip_address=192.168.2.2/24' >> /etc/dhcpcd.conf
+    updated=1
+fi
+
+if ! grep -xF 'interface=eth0' /etc/dnsmasq.conf
+then
+	echo 'interface=eth0' >> /etc/dnsmasq.conf
+	echo 'bind-dynamic' >> /etc/dnsmasq.conf
+	echo 'domain-needed' >> /etc/dnsmasq.conf
+	echo 'bogus-priv' >> /etc/dnsmasq.conf
+	echo 'dhcp-range=192.168.2.100,192.168.2.200,255.255.255.0,24h' >> /etc/dnsmasq.conf
+    updated=1
+fi
+
 if [[ $updated -eq 1 ]]
 then
     sudo reboot
