934f815054b007b8001e4c80ea3332f9f188e352
MuzickMan80 <dale@megndale.com>
2023-11-03 21:36:18 -0500

    +x station.py

--- station/setup.sh
+++ station/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get install -y python3-pygame python3-pip python3-socks
 sudo pip install -r station/requirements.txt
-
+sudo chmod a+x station/station.py
 sudo cp station/station.service /lib/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable station.service
