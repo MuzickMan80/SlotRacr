6e4ded25e56871dbe909b07e7c703467e4aa05cf
MuzickMan80 <dale@megndale.com>
2024-02-25 20:53:49 -0600

    Fix update stations script

--- station/update_stations.sh
+++ station/update_stations.sh
@@ -7,9 +7,9 @@ do
     echo hello $i;
     sshpass -p SlotRacr ssh $OPT slotracr@$i 'sudo systemctl kill station' || true
     sshpass -p SlotRacr scp $OPT -r station slotracr@$i:~
-    sshpass -p SlotRacr ssh $OPT 'sudo timedatectl set-timezone "America/Chicago"'
+    sshpass -p SlotRacr ssh $OPT slotracr@$i 'sudo timedatectl set-timezone "America/Chicago"'
     TIME=$(date +"%y-%m-%d %H:%M:%S")
-    sshpass -p SlotRacr ssh $OPT "sudo timedatectl set-ntp false"
-    sshpass -p SlotRacr ssh $OPT "sudo timedatectl set-time '$TIME'"
+    sshpass -p SlotRacr ssh $OPT slotracr@$i "sudo timedatectl set-ntp false"
+    sshpass -p SlotRacr ssh $OPT slotracr@$i "sudo timedatectl set-time '$TIME'"
     sshpass -p SlotRacr ssh $OPT -R 8080 slotracr@$i 'http_proxy=socks5h://localhost:8080 ~/station/setup.sh'
 done
